cd90d0e84b756fa2d0e1ee28b648141c5788521a
Jblaney <joshua.blaney@priceline.com>
2016-06-03 09:55:22 -0400

    Update 2016-06-03T13:55:18.827Z

--- gulpfile.babel.js
+++
@@ -1,8 +0,0 @@
-import gulp from 'gulp';
-import runSequence from 'run-sequence';
-import buildTasks from './gulp/build';
-import testTasks from './gulp/test';
-
-gulp.task('default', function(done) {
-  runSequence('clean', 'move', 'build', 'watch', done);
-});
